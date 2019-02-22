39c8444a84f603264bc88bf2a24e24b248e4074f
TKDefender <tkdefender88@gmail.com>
2019-02-22 13:27:34 -0700

    starting oauth

--- main.go
+++ main.go
@@ -7,11 +7,24 @@ import (
 	"github.com/Tkdefender88/ButteAir/aq"
 )
 
+var (
+	config = oauth2.Config{
+		ClientID:     "222222",
+		ClientSecret: "222222",
+		Scope:        []string{"all"},
+		RedirectURL:  "http://localhost:9000/oauth2",
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "http://localhost:9000/authorize",
+			TokenURL: "http://localhost:9000/token",
+		},
+	}
+)
+
 func main() {
 	fs := http.FileServer(http.Dir("./assets"))
 	http.HandleFunc("/", index)
 	http.HandleFunc("/airqual", aq.Index)
-	http.Handle("/resources/", http.StripPrefix("/resources", fs))
+	http.Handle("/assets/", http.StripPrefix("/assets", fs))
 
 	log.Println("Listening ...")
 	log.Fatal(http.ListenAndServe(":9000", nil))
