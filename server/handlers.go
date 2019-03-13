9daadf58e976b6a5fffc82d29ffb45fc71e21b67
TKDefender <tkdefender88@gmail.com>
2019-03-13 10:22:18 -0600

    added compliance for the api specified in the meeting

--- server/handlers.go
+++ server/handlers.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -19,6 +18,37 @@ type quality struct {
 	Pm3      int
 }
 
+//Airinfo represents a json object that would be recieved from the device about
+//an update to the airquality at a location
+type Airinfo struct {
+	DeviceID string         `json:"deviceID"`
+	Name     string         `json:"name"`
+	Temp     string         `json:"temp"`
+	Humidity string         `json:"humidity"`
+	PM10     string         `json:"pm10"`
+	PM25     string         `json:"pm25"`
+	PM100    string         `json:"pm100"`
+	Location Location       `json:"location"`
+	Time     CollectionTime `json:"now"`
+}
+
+//CollectionTime is the time and date when a sample was collected
+type CollectionTime struct {
+	Time string `json:"time"`
+	Date string `json:"date"`
+}
+
+//Location is a representation of where each sampling device is placed
+//represented by latitude and longitude
+type Location struct {
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
+}
+
+var (
+	devices = make(map[string]*Airinfo)
+)
+
 // Index redirects to the airqual page
 func Index(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/airqual", http.StatusSeeOther)
@@ -32,92 +62,13 @@ func NotImplemented(w http.ResponseWriter, r *http.Request) {
 
 // AirQuality serves airquality page
 func AirQuality(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, http.StatusText(405), 405)
-		return
-	}
-
-	qual := quality{
-		"here",
-		0,
-		40,
-		10000,
-		200,
-		300,
-	}
-
-	quals := []quality{
-		qual,
-		quality{
-			"airport",
-			20,
-			40,
-			110,
-			210,
-			310,
-		},
-		quality{
-			"hospital",
-			30,
-			40,
-			120,
-			220,
-			320,
-		},
-		quality{
-			"Whale-Mart",
-			30,
-			40,
-			120,
-			220,
-			320,
-		},
-		quality{
-			"Sooubway",
-			30,
-			40,
-			120,
-			220,
-			320,
-		},
-		quality{
-			"Church",
-			30,
-			40,
-			120,
-			220,
-			320,
-		},
-	}
-
-	config.TPL.ExecuteTemplate(w, "airqual.html", quals)
-}
-
-type airinfo struct {
-	DeviceID string         `json:"deviceID"`
-	Temp     string         `json:"temp"`
-	Humidity string         `json:"humidity"`
-	PM10     string         `json:"pm1"`
-	PM25     string         `json:"pm25"`
-	PM100    string         `json:"pm100"`
-	Location location       `json:"location"`
-	Time     collectionTime `json:"now"`
-}
-
-type collectionTime struct {
-	Time string `json:"time"`
-	Date string `json:"date"`
-}
-
-type location struct {
-	Lat  float64 `json:"lat"`
-	Long float64 `json:"long"`
+	config.TPL.ExecuteTemplate(w, "airqual.html", devices)
 }
 
 // UpdateData is where POST requests are recieved from the dragino radio with
 // new data
 func UpdateData(w http.ResponseWriter, r *http.Request) {
-	var info airinfo
+	var info Airinfo
 
 	err := json.NewDecoder(r.Body).Decode(&info)
 	if err != nil {
@@ -136,5 +87,5 @@ func UpdateData(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	fmt.Fprintf(w, info.Humidity)
+	devices[info.Name] = &info
 }
--- server/middleware.go
+++ server/middleware.go
@@ -16,7 +16,6 @@ func isAuthorized(endpoint http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Header["Token"] != nil {
-
 			token, err := jwt.Parse(r.Header["Token"][0],
 				func(token *jwt.Token) (interface{}, error) {
 					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -26,6 +25,7 @@ func isAuthorized(endpoint http.Handler) http.Handler {
 				},
 			)
 			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
 				fmt.Fprintf(w, err.Error())
 			}
 
@@ -33,6 +33,7 @@ func isAuthorized(endpoint http.Handler) http.Handler {
 				endpoint.ServeHTTP(w, r)
 			}
 		} else {
+			w.WriteHeader(http.StatusUnauthorized)
 			fmt.Fprintf(w, "Not Authorized")
 		}
 	})
